3513e4cde11507abd212404d626829166977bb05
Amish Sharma <sharmaamish39@gmail.com>
2021-12-29 23:36:02 +0530

    Time: 16 ms (88.46%), Space: 16.8 MB (65.94%) - LeetHub

---
+++ populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    void hepler(Node* root) {
+        if (!root) return;
+        if (root->left) {
+            root->left->next = root->right;
+        }
+        if (root->next && root->right) {
+            root->right->next = root->next->left;
+        }
+        hepler(root->left);
+        hepler(root->right);
+    }
+    Node* connect(Node* root) {
+        hepler(root);
+        return root;
+    }
+};
