a6633df70e1c7a48814abd8c00f323eb1daf007c
Amish Sharma <sharmaamish39@gmail.com>
2022-01-09 01:23:54 +0530

    Time: 24 ms (97.52%), Space: 9.8 MB (76.35%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -3,10 +3,30 @@ private:
     int dp[70][70][70];
     int n, m;
 public:
+    // My code (AC): 
+    // int func(int i, int j, int y, vector<vector<int>>& v) {
+    //     if (i < 0 || i >= n || j < 0 || j >= m || y < 0 || y >= m) return 0;
+    //     if (dp[i][j][y] != -1) return dp[i][j][y];
+    //     int temp = max({func(i+1, j-1, y-1, v) , func(i+1, j-1, y, v) , func(i+1, j-1, y+1, v) , func(i+1, j, y-1, v), func(i+1, j, y, v) , func(i+1, j, y+1, v) , func(i+1, j+1, y-1, v) , func(i+1, j+1, y, v) , func(i+1, j+1, y+1, v)});
+    //     if (j == y) {
+    //         return dp[i][j][y] = v[i][j] + temp;
+    //     }
+    //     return dp[i][j][y] = v[i][j] + v[i][y] + temp;
+    // }
+    
+    // After code standardization :-
     int func(int i, int j, int y, vector<vector<int>>& v) {
-        if (i < 0 || i >= n || j < 0 || j >= m || y < 0 || y >= m) return 0;
+        if (i == n) return 0;
         if (dp[i][j][y] != -1) return dp[i][j][y];
-        int temp = max({func(i+1, j-1, y-1, v) , func(i+1, j-1, y, v) , func(i+1, j-1, y+1, v) , func(i+1, j, y-1, v), func(i+1, j, y, v) , func(i+1, j, y+1, v) , func(i+1, j+1, y-1, v) , func(i+1, j+1, y, v) , func(i+1, j+1, y+1, v)});
+        int temp = 0;
+        for (int p = -1; p <= 1; p++) {
+            for (int q = -1; q <= 1; q++) {
+                int nc1 = j + p, nc2 = y + q;
+                if (nc1 >= 0 && nc1 < m && nc2 >= 0 && nc2 < m) {
+                    temp = max(temp, func(i+1, nc1, nc2, v));
+                }
+            }
+        }
         if (j == y) {
             return dp[i][j][y] = v[i][j] + temp;
         }
