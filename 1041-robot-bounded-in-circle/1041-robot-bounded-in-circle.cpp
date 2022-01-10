37fb19fbefb9851502890ad32ce3cb22a5aaf96a
Amish Sharma <sharmaamish39@gmail.com>
2022-01-11 01:08:29 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (20.23%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
-    bool isRobotBounded(string instructions) {
-        int x = 0, y = 0, i = 0;
-        vector<vector<int>> d = {{0, 1}, {1, 0}, {0, -1}, { -1, 0}};
-        for (char & ins : instructions)
-            if (ins == 'R')
-                i = (i + 1) % 4;
-            else if (ins == 'L')
-                i = (i + 3) % 4;
-            else
-                x += d[i][0], y += d[i][1];
-        return x == 0 && y == 0 || i > 0;
+    bool isRobotBounded(string str) {
+        int x = 0, y = 0;
+        char dir = 'N';
+        for(int i=0;i<str.length();i++) {
+            if (str[i] == 'G') {
+                if (dir == 'N') y++;
+                if (dir == 'S') y--;
+                if (dir == 'W') x--;
+                if (dir == 'E') x++;
+            } else if (str[i] == 'L') {
+                if (dir == 'N') dir = 'W';
+                else if (dir == 'S') dir = 'E';
+                else if (dir == 'W') dir = 'S';
+                else if (dir == 'E') dir = 'N';
+            } else {
+                if (dir == 'N') dir = 'E';
+                else if (dir == 'S') dir = 'W';
+                else if (dir == 'W') dir = 'N';
+                else if (dir == 'E') dir = 'S';                
+            }
+        }
+        return (x == 0 && y == 0) || dir != 'N';
     }
 };
