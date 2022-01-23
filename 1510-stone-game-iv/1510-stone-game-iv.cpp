a15ec00d66f01adef626efb5cbe874cc877d9619
Amish Sharma <sharmaamish39@gmail.com>
2022-01-23 17:47:22 +0530

    Time: 307 ms (22.44%), Space: 39.8 MB (6.60%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    vector<long long int> v;
+    int dp[2][100001];
+public:
+    bool func(int target, bool ch) {
+        if (dp[ch][target] != -1) return dp[ch][target];
+        int a = 0;
+        for(auto it:v) {
+            if (target >= it) {
+                a += !func(target-it, !ch);
+                if (a > 0) break;
+            }
+        }
+        // if (ch) {
+        //     if (a == false) return true;
+        //     else return false;
+        // } else {
+        //     if (a == true) return false;
+        //     else return true;
+        // }
+        return dp[ch][target] = a;
+    }
+    bool winnerSquareGame(int n) {
+        if (n == 1) return true;
+        if (n == 2) return false;
+        
+        for(long long int i=1;i<=n;i++) {
+            if (i * i > n) break;
+            v.push_back(1ll*i*i);
+        }
+        memset(dp, -1, sizeof dp);
+        return func(n, true);
+    }
+};
