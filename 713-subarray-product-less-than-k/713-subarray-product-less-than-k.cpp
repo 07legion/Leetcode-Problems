002a72f5a93d1ecc127c254eefb2685f0a2fca72
Amish Sharma <sharmaamish39@gmail.com>
2022-01-27 00:09:46 +0530

    Time: 124 ms (33.97%), Space: 61.3 MB (70.24%) - LeetHub

--- 713-subarray-product-less-than-k/713-subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& v, int k) {
-        int low = 0, high = 0, ans = 0, n = v.size();
-        int prod = 1;
+        int low = 0, high = 0, ans = 0, n = v.size(), prod = 1;
         while(low < n && high < n) {
             if (prod * v[high] < k) {
                 prod *= v[high];
