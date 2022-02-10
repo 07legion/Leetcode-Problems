07dab810e8cdad2861f477ab3d8ef1ec97958a5f
Amish Sharma <sharmaamish39@gmail.com>
2022-02-11 01:02:21 +0530

    Time: 198 ms (9.80%), Space: 45.5 MB (8.27%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,13 +1,34 @@
 class Solution {
 public:
     int subarraySum(vector<int>& v, int k) {
+        int n = v.size();
+        vector<int> pre(n, 0);
+        int ans = 0;
+        pre[0] = v[0];
+        for(int i=1;i<n;i++) pre[i] = pre[i-1] + v[i];
+        // int low = 0, high = 0;
+        // // for(auto it:pre) cout<<it<<" "; cout<<"\n";
+        // while(low < n && high < n) {
+        //     int sm = 0;
+        //     if (low == 0) sm += pre[high];
+        //     else sm += pre[high] - pre[low-1];
+        //     // cout<<low<<" "<<high<<" "<<sm<<"\n";
+        //     if (sm >= k) {
+        //         if (sm == k)
+        //             ans++;
+        //         low++;
+        //     } else {
+        //         high++;
+        //     }
+        //     if (low > high) high = low;
+        // }
         map<int, int> mp;
-        int a = 0, ans = 0;
-        mp[0] = 1;
-        for(int i=0;i<v.size();i++) {
-            a += v[i];
-            ans += mp[a - k];
-            mp[a]++;
+        mp[0] = true;
+        for(int i=0;i<n;i++) {
+            if (mp[pre[i]-k]) {
+                ans += mp[pre[i]-k];
+            }
+            mp[pre[i]]++;
         }
         return ans;
     }
