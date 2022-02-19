4ccd4a47ee740d4eaa4988004be674f7cd635ae1
Amish Sharma <sharmaamish39@gmail.com>
2022-02-19 22:52:16 +0530

    Time: 277 ms (60.93%), Space: 71.3 MB (83.44%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -10,13 +10,16 @@ public:
             mn = min(mn, it);
             pq.push(it);
         }
+        ans = min(ans, pq.top() - mn);
         while(pq.top() % 2 == 0) {
-            ans = min(ans, pq.top() - mn);
+            // ans = min(ans, pq.top() - mn);
             int a = pq.top();
             pq.push(a / 2);
             mn = min(mn, a / 2);
             pq.pop();
+            ans = min(ans, pq.top() - mn);
         }
-        return min(ans, pq.top() - mn);
+        // return min(ans, pq.top() - mn);
+        return ans;
     }
 };
