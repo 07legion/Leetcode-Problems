f902a79d739224e7869276a5de5650a10e8a4042
Amish Sharma <sharmaamish39@gmail.com>
2022-03-01 22:58:37 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (54.80%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -9,7 +9,15 @@ public:
         return dp[target] = func(target - 1) + func(target - 2);
     }
     int climbStairs(int n) {
-        dp.resize(46, -1);
-        return func(n);
+        // dp.resize(46, -1);
+        // return func(n);
+        if (n <= 3) return n;
+        int f = 3, s = 2;
+        for(int i=4;i<=n;i++) {
+            int temp = f;
+            f = f + s;
+            s = temp;
+        }
+        return f;
     }
 };
