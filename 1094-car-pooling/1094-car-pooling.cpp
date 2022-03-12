ee3456bf9f44590556457593a461a1dbb9bd5ceb
Amish Sharma <sharmaamish39@gmail.com>
2022-03-12 18:29:43 +0530

    Time: 14 ms (79.51%), Space: 10.5 MB (49.65%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,33 +1,13 @@
 class Solution {
 public:
-    // static bool cmp(const vector<int>& v1, const vector<int>& v2) {
-    //     return (v1[1] < v2[1] || (v1[1] == v2[1] && v1[2] < v2[2]));
-    // }
     bool carPooling(vector<vector<int>>& v, int capacity) {
-        // sort(v.begin(), v.end(), cmp);
-        // for(auto it:v) {
-        //     for(auto it1:it) cout<<it1<<" "; cout<<"\n";
-        // }
-        vector<pair<int, int> > a, b;
-        for(int i=0;i<v.size();i++) {
-            a.push_back({v[i][1], v[i][0]});
-            b.push_back({v[i][2], v[i][0]});
+        int a[1010]; memset(a, 0, sizeof a);
+        for(auto it:v) a[it[1]] += it[0], a[it[2]] -= it[0];
+        int sm = 0;
+        for(auto it:a) {
+            sm += it;
+            if (sm > capacity) return false;
         }
-        sort(a.begin(), a.end());
-        sort(b.begin(), b.end());
-        int n = v.size();
-        int i = 0, j = 0;
-        int ans = 0, t = 0;
-        while(i < n && j < n) {
-            if (a[i].first < b[j].first) {
-                t += a[i].second;
-                i++;
-            } else {
-                t -= b[j].second;
-                j++;
-            }
-            ans = max(ans, t);
-        }
-        return capacity >= ans;
+        return true;
     }
 };
