44e5c70c14f237a7bbf836bc5de3450a046a0d06
Amish Sharma <sharmaamish39@gmail.com>
2022-03-15 13:38:04 +0530

    Time: 12 ms (32.18%), Space: 13 MB (87.03%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& v) {
+        
+        int n = v.size(), ans = 0;
+        for(int i=0;i<n;i++) {
+            // find valley to buy
+            while(i < n - 1 && v[i] >= v[i+1]) {
+                i++;
+            }
+            int valley = v[i];
+            while(i < n - 1 && v[i] <= v[i+1]) {
+                i++;
+            }
+            int peak = v[i];
+            ans += peak - valley;
+        }
+        return ans;
+    }
+};
+    
+
+       
