5730ffc06f2760e581f870d33582bf14996ad3bf
Amish Sharma <sharmaamish39@gmail.com>
2022-03-17 00:24:42 +0530

    Time: 25 ms (41.10%), Space: 14.2 MB (59.44%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -12,27 +12,39 @@ class Solution {
 private:
     node* ans;
 public:
-    bool func(node* root, node* p, node* q) {
-        if (!root || !p || !q) return false;
+//     bool func(node* root, node* p, node* q) {
+//         if (!root || !p || !q) return false;
         
-        bool a = func(root->left, p, q);
-        bool b = func(root->right, p, q);
+//         if (root->val == p->val || root->val == q->val) return true;
         
-        if (a && b) {
-            ans = root; 
-            return true;
-        }
-        if ((root->val == p->val || root->val == q->val) && (a || b)) {
-            ans = root;
-            return true;
-        }
-        if (a || b || root->val == p->val || root->val == q->val) 
-            return true;
-        return false;
+//         bool a = func(root->left, p, q);
+//         bool b = func(root->right, p, q);
+        
+//         if (a && b) {
+//             ans = root; 
+//             return true;
+//         }
+//         if ((root->val == p->val || root->val == q->val) && (a || b)) {
+//             ans = root;
+//             return true;
+//         }
+//         if (a || b || root->val == p->val || root->val == q->val) 
+//             return true;
+//         return false;
+//     }
+    node* func(node* root, node* p, node* q) {
+        if (!root || root == p || root == q) return root; 
+        
+        node* left = func(root->left, p, q);
+        node* right = func(root->right, p, q);
+        
+        if (left == NULL) return right;
+        else if  (right == NULL) return left;
+        else return root;
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         ans = NULL;
-        func(root, p, q);
+        return func(root, p, q);
         return ans;
     }
 };
