02e2ce617b843df57ab43ab6b99a338c6c980aef
Amish Sharma <sharmaamish39@gmail.com>
2022-03-24 12:55:30 +0530

    Time: 201 ms (28.71%), Space: 19.8 MB (96.24%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -5,18 +5,19 @@ public:
         int n = v.size();
         sort(v.begin(), v.end());
         for(int i=0;i<n-2;i++) {
-            if (i > 0 && v[i] == v[i-1]) continue;
-            int low = i + 1, high = n-1;
+            if (i && v[i] == v[i-1]) continue;
+            int low = i+1, high = n - 1;
             while(low < high) {
-                if (v[low] + v[high] == -v[i]) {
+                if (v[low] + v[high] + v[i] == 0) {
                     ans.push_back({v[i], v[low], v[high]});
-                    while(low + 1 <= n - 1 && low < high && v[low] == v[low + 1]) low++;
-                    while(high - 1 >= 0 && low < high && v[high] == v[high - 1]) high--;
+                    while(low + 1 < n && v[low] == v[low + 1]) low++;
+                    while(high - 1 >= 0 && v[high] == v[high-1]) high--;
                     low++; high--;
-                } else if (v[low] + v[high] < -v[i]) low++;
+                }
+                else if (v[low] + v[high] + v[i] < 0) low++;
                 else high--;
             }
         }
-        return ans;
+        return ans;        
     }
 };
