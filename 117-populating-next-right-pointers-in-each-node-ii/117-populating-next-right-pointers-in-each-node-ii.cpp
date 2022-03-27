f6ecc33e42f0d466291e6921a8ebbf2ac474ca32
Amish Sharma <sharmaamish39@gmail.com>
2022-03-27 16:37:01 +0530

    Time: 12 ms (87.85%), Space: 17.6 MB (17.61%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -34,6 +34,7 @@ public:
                 if (temp->left) q.push(temp->left);
                 if (temp->right) q.push(temp->right);
             }
+            prev->next = NULL;
         }
         return root;
     }
