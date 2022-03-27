1e6ace6ab37640783b453e22af3c8d598ad49a46
Amish Sharma <sharmaamish39@gmail.com>
2022-03-27 12:10:20 +0530

    Time: 1464 ms (8.75%), Space: 9.4 MB (85.68%) - LeetHub

---
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
@@ -0,0 +1,37 @@
+class Solution {
+private:
+    vector<int> v;
+    int k;
+    vector<bool> vis;
+public:
+    bool func(int i, int a, int level, int target) {
+        if (level == k-1) {
+            return true;
+        }
+        if (i >= v.size()) return false;
+        if (a == target) {
+            return func(0, 0, level+1, target);
+        }
+        for(int j=i;j<v.size();j++) {
+            if (!vis[j] && a + v[j] <= target) {
+                vis[j] = true;
+                bool res;
+                // if (a + v[j] == target) res = func(0, 0, level+1, target);
+                // else res = 
+                res = func(j+1, a + v[j], level, target);
+                if (res) return true;
+                vis[j] = false;
+            }
+        }
+        return false;
+    }
+    bool canPartitionKSubsets(vector<int>& v1, int K) {
+        v = v1; k = K;
+        int sm = 0;
+        for(auto it:v) sm += it;
+        if (sm < k || sm % k) return false;
+        vis.resize(v1.size(), false);
+        sort(v.begin(), v.end(), greater<int> ());
+        return func(0, 0, 0, sm/k);
+    }
+};
