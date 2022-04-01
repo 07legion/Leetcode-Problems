44ff512c0dadfe3eadbdce91999096f1457e0d5b
Amish Sharma <sharmaamish39@gmail.com>
2022-04-02 01:27:14 +0530

    Time: 12 ms (95.67%), Space: 26.6 MB (16.01%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -13,26 +13,24 @@
 using node = TreeNode;
 class Solution {
 private:
+    unordered_map<int, int> mp;
     vector<int> pre, in;
     int n, a;
 public:
     node* func(int start, int end) {
         if (start > end) return NULL;
         node* root = new node();
-        for(int j=start;j<=end;j++) {
-            if (in[j] == pre[a]) {
-                root->val = pre[a++];
-                root->left = func(start, j-1);
-                root->right = func(j+1, end);
-                break;
-            }
-        }
+        int val = pre[a++];
+        root->val = val;
+        root->left = func(start, mp[val]-1);
+        root->right = func(mp[val]+1, end);
         return root;
     }
     TreeNode* buildTree(vector<int>& pre1, vector<int>& in1) {
         in = in1; pre = pre1;
         n = in.size();
         a = 0;
+        for(int i=0;i<n;i++) mp[in[i]] = i;
         return func(0, n-1);
     }
 };
