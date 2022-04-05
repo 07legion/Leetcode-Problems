2d52c6ced3b253670a242c36cc01dace1a370b00
Amish Sharma <sharmaamish39@gmail.com>
2022-04-06 00:04:39 +0530

    Time: 16 ms (85.22%), Space: 16.5 MB (81.28%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int jump(vector<int>& v) {
         if (v.size() == 1) return 0;
-        int n = v.size(), ans = 0;
-        int l = 0, r = 0;
+        int n = v.size();
+        int l = 0, r = 0, ans = 0;
         while(r < n-1) {
             int farthest = 0;
-            for(int j=l;j<=r;j++) farthest = max(farthest, j + v[j]);
+            for(int j=l;j<=r;j++) 
+                farthest = max(farthest, j + v[j]);
             l = r + 1;
             r = farthest;
             ans++;
