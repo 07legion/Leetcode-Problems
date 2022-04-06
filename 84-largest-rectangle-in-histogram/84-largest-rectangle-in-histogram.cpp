5e1a68fdedbb0fe5272d1fa824b22e7efa5ed44f
Amish Sharma <sharmaamish39@gmail.com>
2022-04-06 10:26:31 +0530

    Time: 200 ms (72.70%), Space: 107.5 MB (5.03%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& v) {
-        vector<pair<int, int> > nsr, nsl;
-        stack<pair<int, int> > st; // < value, index > 
+        vector<pair<int, int> > nsl, nsr;
+        stack<pair<int, int> > st;
         int n = v.size();
         for(int i=n-1;i>=0;i--) {
-            if (st.empty()) {
-                nsr.push_back({-1, v.size()});
-            } else {
+            if (st.empty()) nsr.push_back({-1, v.size()});
+            else {
                 while(!st.empty() && st.top().first >= v[i]) {
                     st.pop();
                 }
-                if (st.empty()) {
-                    nsr.push_back({-1, v.size()});
-                } else nsr.push_back({st.top().first, st.top().second});
+                if (st.empty()) nsr.push_back({-1, v.size()});
+                else nsr.push_back({st.top().first, st.top().second});
             }
             st.push({v[i], i});
         }
@@ -21,25 +19,23 @@ public:
         while(!st.empty()) st.pop();
         for(int i=0;i<n;i++) {
             if (st.empty()) {
-                nsl.push_back({-1, -1});
+                nsl.push_back({-1, -1}); 
             } else {
                 while(!st.empty() && st.top().first >= v[i]) {
                     st.pop();
-                }
-                if (st.empty()) {
-                    nsl.push_back({-1, -1});
-                } else {
-                    nsl.push_back({st.top().first, st.top().second});
-                }
+                } 
+                if (st.empty()) nsl.push_back({-1, -1});
+                else nsl.push_back({st.top().first, st.top().second});
             }
             st.push({v[i], i});
         }
         int ans = 0;
         for(int i=0;i<n;i++) {
-            int temp = 0;
-            temp += abs(i - nsl[i].second) * (v[i]);
-            temp += abs(i - nsr[i].second) * (v[i]);
-            ans = max({ans, temp-v[i], v[i]});
+            int a = 0;
+            a += abs(nsl[i].second - i) * v[i];
+            a += abs(i - nsr[i].second) * v[i];
+            a -= v[i];
+            ans = max(ans, a);
         }
         return ans;
     }
