d9de11e9f4f69577de511e95c75109381de8e3ea
Amish Sharma <sharmaamish39@gmail.com>
2022-04-10 00:13:16 +0530

    Time: 25 ms (23.89%), Space: 13.3 MB (6.02%) - LeetHub

---
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    map<string, int> r;
+public:
+    static bool cmp(pair<pair<int, string>, int>& p1, pair<pair<int, string>, int>& p2) {
+        return (p1.first.first > p2.first.first) || (p1.first.first == p2.first.first && p1.second < p2.second);
+    }
+    vector<string> topKFrequent(vector<string>& v, int k) {
+        sort(v.begin(), v.end());
+        for(int i=0;i<v.size();i++) r[v[i]] = i+1;
+        map<string, int> mp;
+        for(auto it:v) mp[it]++;
+        vector<pair<pair<int, string>, int> > vect;
+        for(auto it:mp) vect.push_back({{it.second, it.first}, r[it.first]});
+        sort(vect.begin(), vect.end(), cmp);
+        // for(auto it:vect) {
+            // cout<<it.first.first<<" "<<it.first.second<<"\n";
+        // }
+        vector<string> ans;
+        for(int i=0;i<vect.size() && k--;i++) {
+            ans.push_back(vect[i].first.second);
+        }
+        return ans;
+    }
+};
