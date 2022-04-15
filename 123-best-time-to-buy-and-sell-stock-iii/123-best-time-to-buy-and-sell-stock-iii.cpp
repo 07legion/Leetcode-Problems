40d0c2890d9ed7831267d6230577daf9b30479b1
Amish Sharma <sharmaamish39@gmail.com>
2022-04-15 18:03:55 +0530

    Time: 164 ms (78.91%), Space: 81.4 MB (50.40%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int maxProfit(vector<int>& v) {
-        int n = v.size();
-        vector<int> left(n+1, 0), right(n+1, 0);
-        int lMin = v[0];
+        int ans = 0, n = v.size();
+        vector<int> left(n, 0), right(n, 0);
+        int lMin = v[0], rMax = v[n-1];
         for(int i=1;i<n;i++) {
             left[i] = max(left[i-1], v[i] - lMin);
             lMin = min(lMin, v[i]);
         }
-        int rMax = v[n-1];
         for(int i=n-2;i>=0;i--) {
             right[i] = max(right[i+1], rMax - v[i]);
             rMax = max(rMax, v[i]);
         }
-        int ans = right[0];
-        for(int i=1;i<n;i++) {
-            ans = max(ans, left[i-1] + right[i]);
+        ans = right[0];
+        for(int i=0;i<n-1;i++) {
+            ans = max(ans, left[i] + right[i+1]);
         }
         return ans;
    }
