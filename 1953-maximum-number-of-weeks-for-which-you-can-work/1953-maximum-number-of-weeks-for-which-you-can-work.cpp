557d9b978e9431dc0e93a1c92f6391092453122d
Amish Sharma <sharmaamish39@gmail.com>
2022-04-21 00:39:13 +0530

    Time: 150 ms (82.81%), Space: 75.5 MB (64.70%) - LeetHub

--- 1953-maximum-number-of-weeks-for-which-you-can-work/1953-maximum-number-of-weeks-for-which-you-can-work.cpp
+++ 1953-maximum-number-of-weeks-for-which-you-can-work/1953-maximum-number-of-weeks-for-which-you-can-work.cpp
@@ -2,12 +2,11 @@ typedef long long int ll;
 class Solution {
 public:
     long long numberOfWeeks(vector<int>& v) {
+        int n = v.size(); 
         ll sm = 0, mx = INT_MIN;
-        for(int i=0;i<v.size();i++) sm += v[i], mx = max(mx, ll(v[i]));
+        for(int i=0;i<n;i++) mx = max(mx, ll(v[i])), sm += v[i];
         ll rest = sm - mx;
-        if (mx > rest) {
-            return 2 * rest + 1;
-        }
-        return sm;
+        if (mx > rest) return 2 * rest + 1;
+        else return sm;
     }
 }; 
