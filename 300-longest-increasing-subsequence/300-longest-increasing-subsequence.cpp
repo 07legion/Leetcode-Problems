c0671065a3d07dcf7fdbe6326c5fd7f1f01e74e6
Amish Sharma <sharmaamish39@gmail.com>
2022-04-20 19:10:29 +0530

    Time: 354 ms (40.09%), Space: 10.5 MB (40.74%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& v) {
-        int n = v.size();
-        vector<int> ans;
-        for(int i=0;i<n;i++) {
-            if (ans.empty()) ans.push_back(v[i]);
-            else {
-                if (v[i] > ans.back()) ans.push_back(v[i]);
-                else {
-                    int index = lower_bound(ans.begin(), ans.end(), v[i]) - ans.begin();
-                    ans[index] = v[i];
-                }
-            }
-        }
-        return ans.size();
-        // int n = v.size();        
-        // vector<int> lis(n);
-        // lis[0] = 1;
-        // for(int i=1;i<n;i++) {
-        //     lis[i] = 1;
-        //     for(int j=0;j<i;j++) {
-        //         if (v[i] > v[j] && lis[i] < lis[j] + 1) {
-        //             lis[i] = lis[j] + 1;
+        // int n = v.size();
+        // vector<int> ans;
+        // for(int i=0;i<n;i++) {
+        //     if (ans.empty()) ans.push_back(v[i]);
+        //     else {
+        //         if (v[i] > ans.back()) ans.push_back(v[i]);
+        //         else {
+        //             int index = lower_bound(ans.begin(), ans.end(), v[i]) - ans.begin();
+        //             ans[index] = v[i];
         //         }
         //     }
         // }
-        // int mx = INT_MIN;
-        // for(auto it:lis) mx = max(mx, it);
-        // return mx;
+        // return ans.size();
+        
+        int n = v.size();
+        vector<int> lis(n);
+        int mx = INT_MIN;
+        lis[0] = 1;
+        for(int i=1;i<n;i++) {
+            lis[i] = 1;
+            for(int j=0;j<i;j++) {
+                if (v[i] > v[j] && lis[i] < lis[j] + 1) {
+                    lis[i] = lis[j] + 1;
+                }
+            }
+            // mx = max(mx, lis[i]);
+        }
+        for(auto it:lis) mx = max(mx, it);
+        return mx;
     }
 };
