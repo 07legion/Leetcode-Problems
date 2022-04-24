f692d47bbfc2c27b428ab19df6f20f130ae4a9fd
Amish Sharma <sharmaamish39@gmail.com>
2022-04-24 19:42:14 +0530

    Time: 32 ms (48.77%), Space: 14.3 MB (95.91%) - LeetHub

---
+++ 1377-frog-position-after-t-seconds/1377-frog-position-after-t-seconds.cpp
@@ -0,0 +1,46 @@
+class Solution {
+private:
+    vector<int> v[101];
+    vector<bool> vis;
+    int n, t, target;
+    double ans;
+public:
+    void dfs(int u, int tim, double p) {
+        vis[u] = true;
+        int ct = 0;
+        for(auto it:v[u]) {
+            if (it == u) continue;
+            if (!vis[it]) ct++;
+        }
+        // cout<<u<<" "<<tim<<" "<<ct<<"\n";
+        if (target == u) {
+            if (tim == t || (ct == 0 && tim < t)) {
+                ans = p;
+            } else ans = 0; 
+            return;
+        }
+        for(auto it:v[u]) {
+            if (vis[it]) continue;
+            dfs(it, tim + 1, ((p == 0) ? 1.0 / ct : p * (1.0 / ct)));
+        }
+    }
+    double frogPosition(int N, vector<vector<int>>& edges, int T, int Tar) {
+        n = N; t = T; target = Tar;
+        for(int i=0;i<edges.size();i++) {
+            v[edges[i][0]].push_back(edges[i][1]);
+            v[edges[i][1]].push_back(edges[i][0]);
+        }
+        if (edges.size() == 0) return 1.00000;
+        vis.resize(n+1, false);
+        ans = 0;
+        dfs(1, 0, 0.0);
+        return ans;
+    }
+};
+
+
+
+
+// 2 - 1
+// |
+// 3    
