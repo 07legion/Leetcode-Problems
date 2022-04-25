9314cecf73669962b7172085c2d1dd791b325981
Amish Sharma <sharmaamish39@gmail.com>
2022-04-25 22:19:40 +0530

    Time: 4 ms (81.62%), Space: 16.4 MB (74.85%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -15,7 +15,7 @@ public:
     bool func(node* root1, node* root2) {
         if (!root1 && !root2) return true;
         if (!root1 || !root2) return false;
-        return root1->val == root2->val && func(root1->right, root2->left) && func(root1->left, root2->right);
+        return root1->val == root2->val && func(root1->left, root2->right) && func(root1->right, root2->left);
     }
     bool isSymmetric(TreeNode* root) {
         return func(root, root);
