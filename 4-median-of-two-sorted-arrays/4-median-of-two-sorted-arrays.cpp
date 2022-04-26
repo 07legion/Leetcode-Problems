1d8a092bc9ccc6bf9159c741665fb53a35c166b0
Amish Sharma <sharmaamish39@gmail.com>
2022-04-26 22:41:56 +0530

    Time: 68 ms (24.76%), Space: 89.4 MB (64.07%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& v1, vector<int>& v2) {
-        if (v2.size() < v1.size()) 
-            return findMedianSortedArrays(v2, v1);
+        if (v2.size() < v1.size()) {
+            swap(v1, v2);
+        }
         int n = v1.size(), m = v2.size();
         int low = 0, high = n;
         while(low <= high) {
-            
             int cut1 = low + (high - low) / 2;
             int cut2 = (n + m + 1) / 2 - cut1;
-            
-            int l1 = cut1 == 0 ? INT_MIN : v1[cut1 - 1];
+            int l1 = cut1 == 0 ? INT_MIN : v1[cut1-1];
             int l2 = cut2 == 0 ? INT_MIN : v2[cut2 - 1];
             int r1 = cut1 == n ? INT_MAX : v1[cut1];
             int r2 = cut2 == m ? INT_MAX : v2[cut2];
             
             if (l1 <= r2 && l2 <= r1) {
                 if ((n + m) % 2) return double(max(l1, l2));
-                else return (max(l1, l2) + min(r1, r2)) / 2.0;
+                else return double(max(l1, l2) + min(r1, r2)) / 2.0;
             } else if (l1 > r2) {
                 high = cut1 - 1;
             } else low = cut1 + 1;
@@ -26,8 +25,7 @@ public:
     }
 };
 
+// cut1 = 4
 
-// 1 2 4 7 8 12 20
-// 3 5 8 13 
-    
-// 1 2 3 4 5 7 8 8 12 13 20
+// 3 6 7 9 10 13
+// 2 5 6 9 11 14 16
