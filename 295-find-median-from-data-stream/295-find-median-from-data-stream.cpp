532183195aa328abf4a4218837413ae8c0027e4d
Amish Sharma <sharmaamish39@gmail.com>
2022-04-26 22:08:09 +0530

    Time: 359 ms (75.78%), Space: 117 MB (90.20%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
@@ -1,27 +1,23 @@
 class MedianFinder {
 private:
     priority_queue<int> mxHeap;
-    priority_queue<int, vector<int> , greater<int> > mnHeap;
+    priority_queue<int, vector<int>, greater<int> > mnHeap;
 public:
     MedianFinder() {
         while(!mnHeap.empty()) mnHeap.pop();
-        while(!mxHeap.empty()) mxHeap.pop();
+        while(!mxHeap.empty()) mxHeap.pop();        
     }
     void addNum(int n) {
-        if (mxHeap.empty() || n < mxHeap.top()) {
+        if (mxHeap.empty() || n <= mxHeap.top()) {
             mxHeap.push(n);
         } else mnHeap.push(n);
-        
-        if (mxHeap.size() > mnHeap.size() + 1) {
-            mnHeap.push(mxHeap.top()), mxHeap.pop();
-        } else if (mnHeap.size() > mxHeap.size() + 1) {
-            mxHeap.push(mnHeap.top()), mnHeap.pop();
-        }        
+        if (mxHeap.size() > mnHeap.size() + 1) mnHeap.push(mxHeap.top()), mxHeap.pop();
+        else if (mxHeap.size() + 1 < mnHeap.size()) mxHeap.push(mnHeap.top()), mnHeap.pop();
     }
     double findMedian() {
-        if (mnHeap.size() == mxHeap.size()) return double(mnHeap.top() + mxHeap.top()) / 2.0;
-        else if (mnHeap.size() > mxHeap.size()) return mnHeap.top();
-        return double(mxHeap.top());
+        if (mnHeap.size() == mxHeap.size()) return (mxHeap.top() + mnHeap.top()) / 2.0;
+        else if (mnHeap.size() < mxHeap.size()) return mxHeap.top();
+        else return mnHeap.top();
     }
 };
 
@@ -30,12 +26,4 @@ public:
  * MedianFinder* obj = new MedianFinder();
  * obj->addNum(num);
  * double param_2 = obj->findMedian();
- */
-
-
-
-
-// mxHeap        mnHeap
-// 1 2             5 10 12
-
-
+ */ 
