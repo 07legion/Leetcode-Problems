05321241c0a9b421e865d2a7264fe610d49d7344
Amish Sharma <sharmaamish39@gmail.com>
2022-04-29 01:39:47 +0530

    Added solution - LeetHub

--- Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
+++ Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
@@ -4,20 +4,38 @@ using namespace std;
 
  // } Driver Code Ends
 class Solution {
-	public:
-	int maxSumIS(int v[], int n)  {  
-        vector<int> a(n, 0);
-        for(int i=0;i<n;i++) a[i] = v[i];
-        for(int i=1;i<n;i++) {
-            for(int j=0;j<i;j++) {
-                if (v[j] < v[i] && a[i] < a[j] + v[i]) {
-                    a[i] = a[j] + v[i];
-                }
+    private:
+        vector<int> v;
+        int dp[1001][1001];
+    public:
+    int func(int prev, int curr) {
+        if (curr == v.size()) return 0;
+        if (prev == -1) {
+            return max(v[curr] + func(curr, curr+1), func(prev, curr+1));
+        } else {
+            if (dp[prev][curr] != -1) return dp[prev][curr];        
+            if (v[curr] > v[prev]) {
+                return dp[prev][curr] = max(v[curr] + func(curr, curr+1), func(prev, curr+1));
             }
+            return dp[prev][curr] = func(prev, curr+1);
         }
-        int mx = INT_MIN;
-        for(int i=0;i<n;i++) mx = max(mx, a[i]);
-        return mx;
+    }
+	int maxSumIS(int v1[], int n)  {  
+	    memset(dp, -1, sizeof dp);
+        for(int i=0;i<n;i++) v.push_back(v1[i]);
+        return func(-1, 0);	    
+        // vector<int> a(n, 0);
+        // for(int i=0;i<n;i++) a[i] = v[i];
+        // for(int i=1;i<n;i++) {
+        //     for(int j=0;j<i;j++) {
+        //         if (v[j] < v[i] && a[i] < a[j] + v[i]) {
+        //             a[i] = a[j] + v[i];
+        //         }
+        //     }
+        // }
+        // int mx = INT_MIN;
+        // for(int i=0;i<n;i++) mx = max(mx, a[i]);
+        // return mx;
 	}  
 };
 
