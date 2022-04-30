d1a845e2e552702672e44037d02357df07882ad2
Amish Sharma <sharmaamish39@gmail.com>
2022-04-30 22:44:16 +0530

    Time: 14 ms (67.00%), Space: 21.7 MB (32.25%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -14,12 +14,29 @@ typedef long long int ll;
 using node = TreeNode;
 class Solution {
 public:
-    bool func(node* root, ll mn, ll mx) {
+    // bool func(node* root, ll mn, ll mx) {
+    //     if (!root) return true;
+    //     return root->val > mn && root->val < mx && func(root->left, mn, root->val) && 
+    //         func(root->right, root->val, mx);
+    // }
+    bool func(node* root, node* &prev) {
         if (!root) return true;
-        return root->val > mn && root->val < mx && func(root->left, mn, root->val) && 
-            func(root->right, root->val, mx);
+        bool a = func(root->left, prev);
+        if (prev && root->val <= prev->val) return false;
+        prev = root;
+        bool b = func(root->right, prev);
+        return a && b;
     }
+    // bool validate(TreeNode* node, TreeNode* &prev) {
+    //     if (node == NULL) return true;
+    //     if (!validate(node->left, prev)) return false;
+    //     if (prev != NULL && prev->val >= node->val) return false;
+    //     prev = node;
+    //     return validate(node->right, prev);
+    // }
     bool isValidBST(TreeNode* root) { 
-        return func(root, LLONG_MIN, LLONG_MAX);
+        node* prev = NULL;
+        return func(root, prev);
+        // return func(root, LLONG_MIN, LLONG_MAX);
     }
 };
