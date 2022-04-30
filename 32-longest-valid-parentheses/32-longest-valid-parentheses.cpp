7ebdfc26491dfca3331c008b79cf456f4861046f
Amish Sharma <sharmaamish39@gmail.com>
2022-04-30 12:53:48 +0530

    Time: 5 ms (54.41%), Space: 6.8 MB (95.76%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,43 +1,23 @@
 class Solution {
 public:
     int longestValidParentheses(string str) {
-        int n = str.length();
-        stack<pair<char, int> > st;
-        int ans = 0;
-        st.push({'.', -1});
+        int n = str.length(), ans = 0;
+        int open = 0, close = 0;
         for(int i=0;i<n;i++) {
-            if (str[i] == '(') {
-                st.push({'(', i});
-            } else {
-                if (!st.empty() && st.top().first != '(') st.push({')', i});
-                else {
-                    if (!st.empty()) {
-                        st.pop();
-                        ans = max(ans, i - st.top().second);
-                    }
-                }
-            }
+            if (str[i] == ')') close++;
+            else open++;
+            if (close == open) {
+                ans = max(ans, close + open);
+            } else if (open < close) open = 0, close = 0;
         }
-        // int mx = 0;
-        // int open = 0, close = 0;
-        // for(int i=0;i<n;i++) {
-        //     if (str[i] == '(') open++;
-        //     else close++;
-        //     if (close > open) {
-        //         open = close = 0;
-        //     } else if (open == close) mx = max(mx, 2 * open);
-        // }
-        // open = close = 0;
-        // for(int i=n-1;i>=0;i--) {
-        //     if (str[i] == '(') open++;
-        //     else close++;
-        //     if (close < open) {
-        //         open = close = 0;
-        //     } else if (open == close) {
-        //         mx = max(mx, 2 * open);
-        //     }
-        // }
-        // return mx;
+        open = close = 0;
+        for(int i=n-1;i>=0;i--) {
+            if (str[i] == ')') close++;
+            else open++;
+            if (close == open) {
+                ans = max(ans, close + open);
+            } else if (open > close) open = 0, close = 0;
+        }
         return ans;
     }
 };
