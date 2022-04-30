284f3b920d37095e2254faadb3fdf317a09f65e8
Amish Sharma <sharmaamish39@gmail.com>
2022-04-30 12:02:04 +0530

    Time: 4 ms (74.05%), Space: 7.7 MB (12.75%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -2,41 +2,42 @@ class Solution {
 public:
     int longestValidParentheses(string str) {
         int n = str.length();
-        // stack<pair<char, int> > st;
-        // int ans = 0;
-        // st.push({'.', -1});
-        // for(int i=0;i<n;i++) {
-        //     if (str[i] == '(') {
-        //         st.push({'(', i});
-        //     } else {
-        //         if (!st.empty() && st.top().first != '(') st.push({')', i});
-        //         else {
-        //             if (!st.empty()) {
-        //                 st.pop();
-        //                 ans = max(ans, i - st.top().second);
-        //             }
-        //         }
-        //     }
-        // }
-        int mx = 0;
-        int open = 0, close = 0;
+        stack<pair<char, int> > st;
+        int ans = 0;
+        st.push({'.', -1});
         for(int i=0;i<n;i++) {
-            if (str[i] == '(') open++;
-            else close++;
-            if (close > open) {
-                open = close = 0;
-            } else if (open == close) mx = max(mx, 2 * open);
-        }
-        open = close = 0;
-        for(int i=n-1;i>=0;i--) {
-            if (str[i] == '(') open++;
-            else close++;
-            if (close < open) {
-                open = close = 0;
-            } else if (open == close) {
-                mx = max(mx, 2 * open);
+            if (str[i] == '(') {
+                st.push({'(', i});
+            } else {
+                if (!st.empty() && st.top().first != '(') st.push({')', i});
+                else {
+                    if (!st.empty()) {
+                        st.pop();
+                        ans = max(ans, i - st.top().second);
+                    }
+                }
             }
         }
-        return mx;
+        // int mx = 0;
+        // int open = 0, close = 0;
+        // for(int i=0;i<n;i++) {
+        //     if (str[i] == '(') open++;
+        //     else close++;
+        //     if (close > open) {
+        //         open = close = 0;
+        //     } else if (open == close) mx = max(mx, 2 * open);
+        // }
+        // open = close = 0;
+        // for(int i=n-1;i>=0;i--) {
+        //     if (str[i] == '(') open++;
+        //     else close++;
+        //     if (close < open) {
+        //         open = close = 0;
+        //     } else if (open == close) {
+        //         mx = max(mx, 2 * open);
+        //     }
+        // }
+        // return mx;
+        return ans;
     }
 };
