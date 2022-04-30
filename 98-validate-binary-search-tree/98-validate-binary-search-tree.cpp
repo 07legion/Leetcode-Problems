43c14ee03d79b6814264543d6577f6087eafd0c7
Amish Sharma <sharmaamish39@gmail.com>
2022-05-01 00:18:28 +0530

    Time: 20 ms (34.39%), Space: 22.2 MB (6.91%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -12,6 +12,11 @@
 
 typedef long long int ll;
 using node = TreeNode;
+struct info {
+    ll mn;
+    ll mx;
+    bool isBst;
+};
 class Solution {
 public:
     // bool func(node* root, ll mn, ll mx) {
@@ -19,17 +24,32 @@ public:
     //     return root->val > mn && root->val < mx && func(root->left, mn, root->val) && 
     //         func(root->right, root->val, mx);
     // }
-    bool func(node* root, node* &prev) {
-        if (!root) return true;
-        bool a = func(root->left, prev);
-        if (prev && root->val <= prev->val) return false;
-        prev = root;
-        bool b = func(root->right, prev);
-        return a && b;
+    
+    // bool func(node* root, node* &prev) {
+    //     if (!root) return true;
+    //     bool a = func(root->left, prev);
+    //     if (prev && root->val <= prev->val) return false;
+    //     prev = root;
+    //     bool b = func(root->right, prev);
+    //     return a && b;
+    // }
+    
+    info func(node* root) {
+        if (!root) return {LLONG_MAX, LLONG_MIN, true};
+        info a = func(root->left);
+        info b = func(root->right);
+        info ans;
+        ans.isBst = a.isBst && b.isBst && root->val > a.mx && root->val < b.mn;
+        ans.mn = min({a.mn, b.mn, ll(root->val)});
+        ans.mx = max({a.mx, b.mx, ll(root->val)});
+        return ans;
     }
+    
     bool isValidBST(TreeNode* root) { 
-        node* prev = NULL;
-        return func(root, prev);
+        info a = func(root); return a.isBst;
+        // node* prev = NULL;
+        // return func(root, prev);
+        
         // return func(root, LLONG_MIN, LLONG_MAX);
     }
 };
