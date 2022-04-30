b11ac38d6e86445e19d6adf759248f0f23d65a52
Amish Sharma <sharmaamish39@gmail.com>
2022-04-30 15:16:24 +0530

    Time: 49 ms (53.66%), Space: 18.9 MB (85.50%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& v) {
-        vector<vector<int> > ans;
         sort(v.begin(), v.end());
         int n = v.size();
-        for(int i=0;i<n;i++) {
-            int j = i;
-            int mn = v[j][0], mx = v[j][1];
-            while(j < n && mx >= v[j][0]) {
-                mn = min(mn, v[j][0]);
-                mx = max(mx, v[j][1]);
-                j++;
+        vector<vector<int> > ans;
+        v.push_back({INT_MIN, INT_MIN});
+        int mn, mx;
+        for(int i=0;i<n+1;i++) {
+            if (i == 0) {
+                mn = v[i][0]; mx = v[i][1];
+            } else {
+                if ((v[i][0] >= mn && v[i][0] <= mx) || (v[i][1] >= mn && v[i][1] <= mx)) {
+                    mn = min(mn, v[i][0]);
+                    mx = max(mx, v[i][1]);
+                } else {
+                    ans.push_back({mn, mx});
+                    mn = v[i][0]; mx = v[i][1];
+                }
             }
-            i = j-1;
-            ans.push_back({mn, mx});
         }
         return ans;
     }
