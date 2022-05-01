941d1f939b446ada7f6994d2b71113ee12543e58
Amish Sharma <sharmaamish39@gmail.com>
2022-05-01 17:27:07 +0530

    Time: 19 ms (93.57%), Space: 16.3 MB (69.93%) - LeetHub

--- 155-min-stack/155-min-stack.cpp
+++ 155-min-stack/155-min-stack.cpp
@@ -1,45 +1,46 @@
-typedef long long int ll;
-class MinStack {
-private:
-    stack<ll> st;
-    ll minElement;
-public:
-    MinStack() {
-        while(!st.empty()) st.pop();
-        minElement = 0;
-    }
+// typedef long long int ll;
+// class MinStack {
+// private:
+//     stack<ll> st;
+//     ll minElement;
+// public:
+//     MinStack() {
+//         while(!st.empty()) st.pop();
+//         minElement = 0;
+//     }
     
-    void push(int val) {
-        if (st.empty()) {
-            minElement = val;
-            st.push(val);
-            return;
-        }
-        if (val < minElement) {
-            st.push(2 * 1ll * val - minElement * 1ll);
-            minElement = val;
-            return;
-        }
-        st.push(val);
-        return;
-    }
+//     void push(int val) {
+//         if (st.empty()) {
+//             minElement = val;
+//             st.push(val);
+//             return;
+//         }
+//         if (val < minElement) {
+//             st.push(2 * 1ll * val - minElement * 1ll);
+//             minElement = val;
+//             return;
+//         }
+//         st.push(val);
+//         return;
+//     }
     
-    void pop() {
-        if (st.top() < minElement) {
-            minElement = 2 * 1ll * minElement - st.top() * 1ll;
-        }
-        st.pop();
-    }
+//     void pop() {
+//         if (st.top() < minElement) {
+//             minElement = 2 * 1ll * minElement - st.top() * 1ll;
+//         }
+//         st.pop();
+//     }
     
-    int top() {
-        if (st.top() < minElement) return minElement;
-        return st.top();
-    }
+//     int top() {
+//         if (st.top() < minElement) return minElement;
+//         return st.top();
+//     }
     
-    int getMin() {
-        return minElement;
-    }
-};
+//     int getMin() {
+//         return minElement;
+//     }
+// };
+
 
 /**
  * Your MinStack object will be instantiated and called as such:
@@ -49,3 +50,41 @@ public:
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
  */
+
+
+struct node {
+    int val;
+    int mn;
+    node* next;
+    node(int v, int m, node* add) {
+        this->val = v;
+        this->mn = m;
+        this->next = add;
+    }
+};
+class MinStack {    
+private:
+    node* head;
+public:
+    MinStack() {
+        head = NULL;
+    }
+    void push(int val) {
+        if (!head) head = new node(val, val, NULL);
+        else {
+            head = new node(val, min(val, head->mn), head);
+        }
+    }
+    
+    void pop() {
+        head = head->next;
+    }
+    
+    int top() {
+        return head->val;
+    }
+    
+    int getMin() {
+        return head->mn;
+    }
+};
