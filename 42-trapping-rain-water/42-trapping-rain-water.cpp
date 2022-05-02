d0a0d823aeec56adb1a50f0874c5bd6fcb94857c
Amish Sharma <sharmaamish39@gmail.com>
2022-05-02 10:01:04 +0530

    Time: 22 ms (32.18%), Space: 17.7 MB (74.48%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,15 +1,35 @@
 class Solution {
 public:
     int trap(vector<int>& v) {
+        
         int n = v.size();
-        vector<int> left(n, v[0]), right(n, v[n-1]);
-        for(int i=1;i<n;i++) left[i] = max(left[i-1], v[i]);
-        for(int i=n-2;i>=0;i--) right[i] = max(right[i+1], v[i]);
-        int ans = 0;
-        for(int i=1;i<n-1;i++) {
-            ans += min(left[i], right[i]) - v[i];
+        
+        int i = 0, j = n-1;
+        int lMax = -1, rMax = -1, ans = 0;
+        
+        while(i <= j) {
+            if (v[i] <= v[j]) { 
+                if (lMax < v[i]) lMax = v[i];
+                else ans += lMax - v[i];
+                i++;
+            } else {
+                if (rMax < v[j]) rMax = v[j];
+                else ans += rMax - v[j];
+                j--;
+            }
         }
         return ans;
+        
+        
+        // int n = v.size();
+        // vector<int> left(n, v[0]), right(n, v[n-1]);
+        // for(int i=1;i<n;i++) left[i] = max(left[i-1], v[i]);
+        // for(int i=n-2;i>=0;i--) right[i] = max(right[i+1], v[i]);
+        // int ans = 0;
+        // for(int i=1;i<n-1;i++) {
+        //     ans += min(left[i], right[i]) - v[i];
+        // }
+        // return ans;
     }
 };
 
