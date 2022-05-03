5be3db17d3dbc03bbea8b123054589d4e5108948
Amish Sharma <sharmaamish39@gmail.com>
2022-05-03 14:50:19 +0530

    Time: 121 ms (22.19%), Space: 16.7 MB (76.17%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int maximalSquare(vector<vector<char>>& v) {
         int n = v.size(), m = v[0].size();
-        int dp[n][m];
-        for(int i=0;i<n;i++) dp[i][0] = v[i][0] - '0';
+        int dp[n+1][m+1];
         for(int i=0;i<m;i++) dp[0][i] = v[0][i] - '0';
+        for(int i=0;i<n;i++) dp[i][0] = v[i][0] - '0';
         for(int i=1;i<n;i++) {
             for(int j=1;j<m;j++) {
-                if (v[i][j] == '1') 
+                if (v[i][j] == '1') {
                     dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
-                else dp[i][j] = 0;
+                } else dp[i][j] = 0;
             }
         }
-        int mx = 0;
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) mx = max(mx, dp[i][j]);
-        }
-        return mx * mx;
+        int ans = 0;
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++) 
+                ans = max(ans, dp[i][j]);
+        return ans * ans;
     }
 };
