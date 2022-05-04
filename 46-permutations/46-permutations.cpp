c30483c529a42e053459c54059567c194064b74c
Amish Sharma <sharmaamish39@gmail.com>
2022-05-04 23:50:20 +0530

    Time: 5 ms (40.81%), Space: 8.6 MB (10.70%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    vector<vector<int> > ans;
+    vector<int> v;
+    vector<bool> vis;
+public:
+    void func(int i, vector<int> temp) {
+        if (i == v.size()) {
+            ans.push_back(temp);
+            return;
+        }
+        for(int j=0;j<v.size();j++) {
+            if (!vis[j]) {
+                vis[j] = true;
+                temp.push_back(v[j]);
+                func(i+1, temp);
+                temp.pop_back();
+                vis[j] = false;
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& V) {
+        v = V;
+        vis.resize(v.size(), false);
+        func(0, vector<int> {});
+        return ans;
+    }
+};
+
+// 1 2 3 4 
