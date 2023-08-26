23264928612c4ff6e9b3583cc8df9d29f8b7a20b
Amish Sharma <sharmaamish39@gmail.com>
2023-08-26 10:20:32 +0530

    Time: 15 ms (99.34%), Space: 10.7 MB (73.71%) - LeetHub

---
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int bestClosingTime(string s) {
+        int n=s.size(), count=0, ans=n;
+        for(int i=n-1; i>=0; i--){
+            count+=(s[i]=='Y')-(s[i]=='N');
+            if(count<=0)ans=i;
+            if(count<0)count=0;
+        }
+        return ans;
+    }
+};
+
+
+
+// NYNYNYYNYNYNNY
+// 01234567890123
